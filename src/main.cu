df9ad202b6a18796f3bc5d2200556df3b480ffad
Harrison Held <harrydheld@gmail.com>
2025-10-20 21:08:55 -0500

    will now test various array sizes

--- src/main.cu
+++ src/main.cu
@@ -9,29 +9,38 @@ void print_array(int* arr, int n) {
     std::cout << "\n";
 }
 
-int main() {
-    const int N = 1 << 24;
-    std::vector<int> arr(N);
-    for (int i = 0; i < N; ++i) arr[i] = rand() % 100000;
+void test_sort_algorithm(const char* name, void(*sort_func)(int*, int), const std::vector<int>& arr) {
+    int* copy = new int[arr.size()];
+    std::copy(arr.begin(), arr.end(), copy);
 
-    int* copy = new int[N];
+    auto start = std::chrono::high_resolution_clock::now();
+    sort_func(copy, arr.size());
+    auto end = std::chrono::high_resolution_clock::now();
 
-    auto test_sort = [&](const char* name, void(*sort_func)(int*, int)) {
-        std::copy(arr.begin(), arr.end(), copy);
-        auto start = std::chrono::high_resolution_clock::now();
-        sort_func(copy, N);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> diff = end - start;
-        std::cout << name << " took " << diff.count() << "s to sort " << N << " elements.\n";
-    };
-
-    test_sort("Merge Sort", merge_sort);
-    test_sort("Quick Sort", quick_sort);
-    test_sort("Radix Sort", radix_sort);
-    test_sort("Bitonic Sort", bitonic_sort);
-    test_sort("Brick Sort", brick_sort);
-    test_sort("CPU Sort", cpu_sort);
+    std::chrono::duration<double> diff = end - start;
+    std::cout << name << " took " << diff.count() 
+              << "s to sort " << arr.size() << " elements.\n";
 
     delete[] copy;
+}
+
+int main() {
+    // array sizes to test
+    std::vector<int> sizes = {1 << 16, 1 << 20, 1 << 24, 1 << 28};
+
+    for (auto n : sizes) {
+        std::cout << "\nCreating array of size " << n << "\n";
+        std::vector<int> arr(n);
+        for (int i = 0; i < n; ++i) 
+            arr[i] = rand() % 100000;
+
+        test_sort_algorithm("CPU std::sort", cpu_sort, arr);
+        test_sort_algorithm("Merge Sort", merge_sort, arr);
+        test_sort_algorithm("Quick Sort", quick_sort, arr);
+        test_sort_algorithm("Radix Sort", radix_sort, arr);
+        test_sort_algorithm("Bitonic Sort", bitonic_sort, arr);
+        test_sort_algorithm("Brick Sort", brick_sort, arr);
+    }
+
     return 0;
 }
